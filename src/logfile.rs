d5e60a359e16bab0bb686e01f1419461be89067e
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-12-31 07:48:39 -0800

    settings
    starting on options and starting the manager

--- src/logfile.rs
+++ src/logfile.rs
@@ -1,3 +1,17 @@
+use std::time::Instant;
+
 pub struct Logfile {
-    
+    created_at: Instant,
+    sealed_at: Option<Instant>,
+    path: String,
+}
+
+impl Logfile {
+    pub fn new(path: String) -> Self {
+        Self {
+            created_at: Instant::now(),
+            sealed_at: None,
+            path,
+        }
+    }
 }
--- src/manager.rs
+++ src/manager.rs
@@ -1,3 +1,33 @@
+use std::collections::{BTreeMap, BTreeSet};
+
+use crate::logfile::Logfile;
+
+/// Manager controls the log file rotation and modification.
+///
+/// A manager is single threaded to ensure maximum throughput for disk operations.
 pub struct Manager {
-    
+    options: ManagerOptions,
+
+    logfiles: BTreeMap<String, Logfile>,
+}
+
+pub struct ManagerOptions {
+    log_dir: String,
+    max_log_size: usize,
+}
+
+impl Manager {
+    pub fn with_options(options: ManagerOptions) -> Self {
+        Self {
+            options,
+            logfiles: BTreeMap::new(),
+        }
+    }
+
+    pub fn start(&self) -> Result<(), std::io::Error> {
+        // Load in all logfiles in the log directory
+        let log_files = std::fs::read_dir(&self.options.log_dir)?;
+
+        Ok(())
+    }
 }
