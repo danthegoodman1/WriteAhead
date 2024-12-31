daa0b43d8f5377ad129f2eaccf202feeec03e164
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-12-31 07:55:26 -0800

    readme
    record ID

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WriteAhead
+
+A partitioned WAL crate for building append-only high throughput durability systems.
+
+A great component for a (distributed) data store.
+
+For distributed usage, consider FDB's model (may want to place a custom in-memory index in front using `crossbeam-skiplist` for custom ID->offset mappings)
--- src/logfile.rs
+++ src/logfile.rs
@@ -6,7 +6,8 @@ use std::time::Instant;
 ///
 /// A log file always starts and ends with the magic bytes (TODO)
 ///
-/// The first bytes after the magic bytes represent the creation timestamp of the log file.
+/// The first bytes after the magic bytes represent the creation timestamp of the log file,
+/// and the file ID (a monotonically increasing integer).
 ///
 /// The last bytes after the magic bytes represent the sealed time of the log file if it exists.
 /// This means that the log file is immutable and cannot be modified.
@@ -22,6 +23,35 @@ impl Logfile {
             created_at: Instant::now(),
             sealed_at: None,
             path,
+        }
+    }
+}
+
+/// RecordID represents a record's location in a log file.
+pub struct RecordID {
+    file_id: u64,
+    file_offset: u64,
+}
+
+impl RecordID {
+    pub fn new(file_id: u64, file_offset: u64) -> Self {
+        Self {
+            file_id,
+            file_offset,
+        }
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        let mut buf = Vec::new();
+        buf.extend_from_slice(&self.file_id.to_le_bytes());
+        buf.extend_from_slice(&self.file_offset.to_le_bytes());
+        buf
+    }
+
+    pub fn deserialize(buf: &[u8]) -> Self {
+        Self {
+            file_id: u64::from_le_bytes(buf[0..8].try_into().unwrap()),
+            file_offset: u64::from_le_bytes(buf[8..16].try_into().unwrap()),
         }
     }
 }
