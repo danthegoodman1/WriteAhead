100e67b7835ceffdb836d3b2fdcbaf89b3da8fb9
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2025-01-01 06:41:53 -0800

    remove directory, add file id

--- src/logfile.rs
+++ src/logfile.rs
@@ -6,23 +6,24 @@ use std::time::Instant;
 ///
 /// A log file always starts and ends with the magic bytes (TODO)
 ///
-/// The first bytes after the magic bytes represent the creation timestamp of the log file,
-/// and the file ID (a monotonically increasing integer).
+/// The name of the file must be the file ID, a monotonically increasing integer
+///
+/// The first bytes after the magic bytes represent the creation timestamp of the log file.
 ///
 /// The last bytes after the magic bytes represent the sealed time of the log file if it exists.
 /// This means that the log file is immutable and cannot be modified.
 pub struct Logfile {
     created_at: Instant,
     sealed_at: Option<Instant>,
-    path: String,
+    id: u64,
 }
 
 impl Logfile {
-    pub fn new(path: String) -> Self {
+    pub fn new(id: u64) -> Self {
         Self {
             created_at: Instant::now(),
             sealed_at: None,
-            path,
+            id,
         }
     }
 }
